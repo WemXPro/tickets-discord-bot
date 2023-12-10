b9d6cedc8dcc9f129631d6d71505e26b45d5db29
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-12-10 03:32:41 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -4,104 +4,108 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 const { Client, GatewayIntentBits, ChannelType, MessageEmbed } = require('discord.js');
 
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-const client = new Client({ 
+const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds, 
-        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent  // Add this intent
-    ] 
+    ]
 });
 
 const botToken = process.env.DISCORD_BOT_TOKEN;
 const APP_URL = process.env.APP_URL;
+const GUILD_ID = process.env.GUILD_ID;
+
+
+
+// Commands
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const rest = new REST({ version: '9' }).setToken(botToken);
+
+const commands = [{
+    name: 'close',
+    description: 'Closes this ticket'
+}, {
+    name: 'echo',
+    description: 'Repeats your message',
+    options: [{
+        name: 'message',
+        type: 3,
+        description: 'Message to repeat',
+        required: true,
+    }],
+}];
+
+
+
+client.once('ready', async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(
+            Routes.applicationGuildCommands(client.user.id, GUILD_ID),
+            { body: commands },
+        );
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    const { commandName } = interaction;
+
+    if (commandName === 'close') {
+        if (!interaction.channel || !interaction.channel.topic) return;
+        let parsedTopic = JSON.parse(interaction.channel.topic);
+
+        if (parsedTopic.hasOwnProperty('ticket_id')) {
+            const api_url_create = APP_URL + '/api/v1/tickets/' + parsedTopic.ticket_id + '/close-or-open';
+            let res = axios.get(api_url_create);
+            await interaction.reply('Ticket closed');
+            return;
+        }
+    } else if (commandName === 'echo') {
+        const message = interaction.options.getString('message');
+        await interaction.reply(`Echo: ${message}`);
+    }
+});
+// End Commands
+
+
 
 client.once('ready', () => {
     console.log('Bot is online!');
     client.user.setActivity('Tickets');
 });
 
-client.on('messageCreate', message => {
-    // Replace this with the appropriate way you get your channel
-    const channel = message.channel;
 
-    // Function to check if channel topic is valid JSON
-    function isTicket(channel) {
-        const topic = channel.topic;
-        
-        if(!message.hasOwnProperty('content')) {
-            return false;
-        }
-
-        if (!topic) {
-            return false;
-        }
-
-        try {
-            // check if data is json
-            const parsedTopic = JSON.parse(topic);
-            if (parsedTopic.hasOwnProperty('ticket_id')) {
-                if(message?.author.bot) {
-                    return false;
-                }
-                let content = message.content;
-
-                const regex = /<@!?(\d+)>/g;
-                content = content.replace(regex, (match, userId) => {
-                    const user = client.users.cache.get(userId);
-                    return user ? `@${user.username}` : match;
-                });
-
-                // Checking if the message has attachments
-                if (message.attachments.size > 0) {
-                    // Go through all attachments
-                    message.attachments.forEach(attachment => {
-                        // Check if the attachment is an image
-                        if (attachment.contentType && attachment.contentType.includes('image')) {
-                            // Add the image URL to the content
-                            content += ' \n' + attachment.url;
-                        }
-                    });
-                }
-
-                const api_url_create = APP_URL + '/api/v1/tickets/'+ parsedTopic.ticket_id + '/discord-message';
-                const data = {
-                    author: message.author.username,
-                    avatar_url: message.author.displayAvatarURL(),
-                    message: content
-                };
-                axios.post(api_url_create, data);
-            }
-
-        } catch (error) {
-            return false;
-        }
-    }
-
-    isTicket(channel);
-});
 
 client.login(botToken);
 
 // Webhook endpoint
 app.post('/webhook', async (req, res) => {
     console.log('Webhook received:', req.body);
-    
+
     const channelName = 'ticket-' + req.body.user.username;
 
     if (!channelName) {
         return res.status(400).send('Channel name not provided in webhook data');
     }
-    
+
     const guild = client.guilds.cache.first();
     if (!guild) {
         return res.status(500).send('Guild not found');
     }
-    
+
     try {
         let topic = {
             ticket_id: req.body.id,
@@ -123,16 +127,16 @@ app.post('/webhook', async (req, res) => {
         });
 
         console.log(`Created new private channel: ${channel.name}`);
-    
+
         // Create a webhook in the new channel and wait for it to be created
         const webhook = await channel.createWebhook({
             name: 'WemX-Webhook',
         });
-    
+
         // Construct the webhook URL
         const webhookUrl = `https://discord.com/api/webhooks/${webhook.id}/${webhook.token}`;
         console.log(`Webhook URL: ${webhookUrl}`);
-    
+
         // Send the webhook URL in the response
         res.status(200).json({ webhookUrl: webhookUrl });
 
