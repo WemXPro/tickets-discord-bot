d03cf71d120d563bdda98837bb6537fd55fecb14
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-12-10 14:52:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ SESSION_NAME="ticket_bot"
 
 # Check if a session already exists
 if screen -list | grep -q "\.${SESSION_NAME}"; then
-    echo "Сесія '${SESSION_NAME}' вже запущена. Переходимо до сесії."
+    echo "Session '${SESSION_NAME}' already started Let's go to the session."
 else
-    echo "Створення та запуск нової сесії '${SESSION_NAME}'."
+    echo "Create and launch a new session '${SESSION_NAME}'."
     # Create and launch a new screen session with the bot
     screen -dmS $SESSION_NAME bash -c 'while true; do node /var/discord/bots/tickets/src/bot.js; echo "Bot crashed, restart after 5 seconds..."; sleep 5; done'
 fi
